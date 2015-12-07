c4baff7d4981331bfea5b303530088088e43cf42
jippylong12 <crazymonkeyshicken@gmail.com>
2015-12-07 17:29:20 +0000

    Set up the NetworkRequest channels.

---
+++ MP6/NetworkRequestChannel.cpp
@@ -0,0 +1,36 @@
+#include "NetworkRequestChannel.h"
+
+
+
+using namespace std;
+
+NetworkRequestChannel::NetworkRequestChannel(const string _server_host_name, const unsigned short _port_no);
+{
+    
+}
+
+
+NetworkRequestChannel::NetworkRequestChannel(const unsigned short _port_no, backlog,  void * (*connection_handler) (int *))
+{
+    
+}
+
+NetworkRequestChannel::~NetworkRequestChannel()
+{
+    
+}
+
+string NetworkRequestChannel::send_request(string _request)
+{
+    
+}
+
+string NetworkRequestChannel::cread()
+{
+    //read(socket, )
+}
+
+int NetworkRequestChannel::cwrite(string _msg)
+{
+    //wriet(socket, functions?)
+}
--- MP6/NetworkRequestChannel.h
+++ MP6/NetworkRequestChannel.h
@@ -1,28 +1,33 @@
-NetworkRequestChannel(const string _server_host_name, const unsigned short _port_no); 
-/* Creates a CLIENT-SIDE local copy of the channel. The channel is connected to 
-the given port number at the given server host.
-THIS CONSTRUCTOR IS CALLED BY THE CLIENT. */ 
-NetworkRequestChannel(const unsigned short _port_no, backlog,  void * (*connection_handler) (int *)); 
-/* Creates a SERVER-SIDE local copy of the channel that is accepting connections 
-at the given port number. NOTE that multiple clients can be connected to the 
-same server-side end of the request channel. Whenever a new connection comes in
-, it is accepted by the server, and the given connection handler is invoked. 
-The parameter to the connection handler is the file descriptor of the slave 
-socket returned by the accept call. NOTE that the connection handler does not 
-want to deal with closing the socket. You will have to close the socket once the
-connection handler is done. */ 
-~NetworkRequestChannel(); 
-/* Destructor of the local copy of the channel. */ 
-string send_request(string _request); 
-/* Send a string over the channel and wait for a reply. */ 
-string cread(); 
-/* Blocking read of data from the channel. Returns a string of characters read 
-from the channel. Returns NULL if read failed. */ 
-int cwrite(string _msg); 
-/* Write the data to the channel. The function returns the number of characters 
-written to the channel. */
-
-//Backlog is when there are multiple clients trying to connect at the same time.
-//While one is being used, if the backlog is zero all connections will be 
-//dropped. If it is not then they will queued. Backlog should not have much 
-//impact and we should be okay with backlog sized 0. 
+class NetworkRequestChannel
+{
+    public:
+    
+    NetworkRequestChannel(const string _server_host_name, const unsigned short _port_no); 
+    /* Creates a CLIENT-SIDE local copy of the channel. The channel is connected to 
+    the given port number at the given server host.
+    THIS CONSTRUCTOR IS CALLED BY THE CLIENT. */ 
+    NetworkRequestChannel(const unsigned short _port_no, backlog,  void * (*connection_handler) (int *)); 
+    /* Creates a SERVER-SIDE local copy of the channel that is accepting connections 
+    at the given port number. NOTE that multiple clients can be connected to the 
+    same server-side end of the request channel. Whenever a new connection comes in
+    , it is accepted by the server, and the given connection handler is invoked. 
+    The parameter to the connection handler is the file descriptor of the slave 
+    socket returned by the accept call. NOTE that the connection handler does not 
+    want to deal with closing the socket. You will have to close the socket once the
+    connection handler is done. */ 
+    ~NetworkRequestChannel(); 
+    /* Destructor of the local copy of the channel. */ 
+    string send_request(string _request); 
+    /* Send a string over the channel and wait for a reply. */ 
+    string cread(); 
+    /* Blocking read of data from the channel. Returns a string of characters read 
+    from the channel. Returns NULL if read failed. */ 
+    int cwrite(string _msg); 
+    /* Write the data to the channel. The function returns the number of characters 
+    written to the channel. */
+    
+    //Backlog is when there are multiple clients trying to connect at the same time.
+    //While one is being used, if the backlog is zero all connections will be 
+    //dropped. If it is not then they will queued. Backlog should not have much 
+    //impact and we should be okay with backlog sized 0. 
+};
